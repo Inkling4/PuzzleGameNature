c2fc521f82aea9f98e4cab989d56bbd7e0a92be8
KirbyWithACoolHat <gransethnerden.gaming@gmail.com>
2025-02-19 17:07:34 +0100

    Made Input files, and some C++ classes
    Didn't put much in them yet though

---
+++ Source/PuzzleGameNature/Public/Protagonist.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputMappingContext.h"
+#include "InputActionValue.h"
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputComponent.h"
+#include "Protagonist.generated.h"
+
+UCLASS()
+class PUZZLEGAMENATURE_API AProtagonist : public ACharacter
+{
+	GENERATED_BODY()
+
+	UPROPERTY(VisibleAnywhere, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* Camera;
+
+public:
+	// Sets default values for this character's properties
+	AProtagonist();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, category = "EnhancedInput")
+	class UInputMappingContext* TheInputMappingContextFile;
+	UPROPERTY(EditAnywhere, category = "EnhancedInput")
+	class UInputAction* IA_Move;
+	UPROPERTY(EditAnywhere, category = "EnhancedInput")
+	class UInputAction* IA_Jump;
+
+	void MoveInput(const FInputActionValue& InputValue);
+	void JumpInput();
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
