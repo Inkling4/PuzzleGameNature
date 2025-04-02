3d6117fa9d628b6fe58df71c306b6004b233a7f7
KirbyWithACoolHat <gransethnerden.gaming@gmail.com>
2025-04-02 13:13:47 +0200

    created pickup classes
    empty for now

---
+++ Source/PuzzleGameNature/Public/CrowbarPickup.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PickupAbleObject.h"
+#include "CrowbarPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEGAMENATURE_API ACrowbarPickup : public APickupAbleObject
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/PuzzleGameNature/Public/MoneyScrapPickup.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PickupAbleObject.h"
+#include "MoneyScrapPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEGAMENATURE_API AMoneyScrapPickup : public APickupAbleObject
+{
+	GENERATED_BODY()
+	
+};
