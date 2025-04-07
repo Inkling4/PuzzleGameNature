f675ad532afc263d4ba2d024f6df978c5eff243b
KirbyWithACoolHat <gransethnerden.gaming@gmail.com>
2025-04-07 14:11:16 +0200

    Made "next level" object

---
+++ Source/PuzzleGameNature/Private/NextLevelInteract.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NextLevelInteract.h"
+
+
+
+
+
+void ANextLevelInteract::Interact()
+{
+	Super::Interact();
+	NextLevel();	
+}
