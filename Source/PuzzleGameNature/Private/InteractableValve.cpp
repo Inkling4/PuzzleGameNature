0b2b060c51646c4cb31549a8b6fc40a2cea0b3a0
Kirbywithacoolhat <gransethnerden.gaming@gmail.com>
2025-04-12 19:31:12 +0200

    Made the valve class

---
+++ Source/PuzzleGameNature/Private/InteractableValve.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InteractableValve.h"
+
+
+//Defaults
+AInteractableValve::AInteractableValve()
+{
+	ActiveValveTime = 3.0;
+	bIsValveActive = false;
+	
+}
+
+void AInteractableValve::Interact()
+{
+	Super::Interact();
+	//Activates valve
+	bIsValveActive = true;
+	//Starts countdown for valve to turn off
+	GetWorld()->GetTimerManager().SetTimer(ValveTimerHandle, this, &AInteractableValve::DisableValve, ActiveValveTime, false);
+	
+}
+
+void AInteractableValve::DisableValve()
+{
+	bIsValveActive = false;
+	GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Blue, TEXT("Valve disabled!"));
+}
